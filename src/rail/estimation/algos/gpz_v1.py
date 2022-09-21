9c0b2f70f54d0e656047cae593893cb4f16290d0
sschmidt23 <sschmidt@physics.ucdavis.edu>
2022-09-21 12:48:14 -0700

    change default iters in params now that code is faster

--- src/rail/estimation/algos/gpz_v1.py
+++ src/rail/estimation/algos/gpz_v1.py
@@ -70,8 +70,8 @@ class Inform_GPz_v1(CatInformer):
                           csl_method=Param(str, "normal", msg="cost sensitive learning type, 'balanced', 'normalized', or 'normal'"),
                           csl_binwidth=Param(float, 0.1, msg="width of bin for 'balanced' cost sensitive learning"),
                           pca_decorrelate=Param(bool, True, msg="if True, decorrelate data using PCA as preprocessing stage"),
-                          max_iter=Param(int, 100, msg="max number of iterations"),
-                          max_attempt=Param(int, 50, msg="max iterations if no progress on validation"),
+                          max_iter=Param(int, 200, msg="max number of iterations"),
+                          max_attempt=Param(int, 100, msg="max iterations if no progress on validation"),
                           log_errors=Param(bool, True, msg="if true, take log of magnitude errors")
                           )
 
